2f94db05c0e9df5881d34e84789d6beeb3e84e56
rimuakter057 <rimu.appdeveloper@gmail.com>
2025-04-28 10:38:01 +0600

    add text

--- lib/app/utils/text/app_text_en.dart
+++ lib/app/utils/text/app_text_en.dart
@@ -1,9 +1,88 @@
 class AppTextEn {
-  /// main screen text
+
+///sign in
+  static const String letsSaveEnvironment = 'Let’s save environment together';
+  static const String signIn = 'Sign In';
+  static const String email = 'Email';
+  static const String userExample = 'user@example.com';
+  static const String password = 'Password';
+  static const String rememberMe = 'Remember me';
+  static const String forgottenPassword = 'Forgotten Password';
+  static const String orSignInWith = 'Or Sign In with';
+
+///send otp
+  static const String sendOtp = 'Send OTP';
+  static const String recoverAccountEasySteps = 'Recover your account in easy steps';
+
+ ///verify otp
+  static const String verifyOtp = 'Verify OTP';
+  static const String anEmailSent = 'An email has been sent to user@example.com. Please enter the sent OTP.';
+  static const String didntReceiveCode = 'Didn’t Receive a code?';
+  static const String resend = 'Resend';
+
+ ///reset password
+  static const String resetPassword = 'Reset Password';
+  static const String newPassword = 'New Password';
+  static const String confirmPassword = 'Confirm Password';
+
+  ///sign up
+  static const String signUp = 'Sign Up';
+  static const String name = 'Name';
+  static const String exampleName = 'e.g: Ahmed Ariyan';
+  static const String phoneNumber = 'Phone Number';
+  static const String alreadyHaveAccount = 'Already have an account?';
+
+/// home
+  static const String goodMorning = 'Good Morning';
+  static const String ahmedAriyan = 'Ahmed Ariyan';
+  static const String healthyEnvironment = 'You are in a healthy environment';
+  static const String myPlaces = 'My Places';
   static const String home = 'Home';
-  static const String salat = 'Salat';
-  static const String alarm = 'Alarm';
-  static const String profile = 'Profile';
-  static const String prayerData="Prayer Data";
-  static const String allPlayerList="All Prayer List";
+  static const String good = 'Good';
+  static const String thirteenPercent = '13%';
+  static const String sixHundredFiftyTwo = '652';
+  static const String ppm = 'ppm';
+  static const String office = 'Office';
+  static const String thirtySevenPercent = '37%';
+  static const String fourHundredFortySeven = '447';
+  static const String viewDetails = 'View Details';
+
+///details
+  static const String history = 'History';
+  static const String nov24 = 'Nov 24';
+  static const String oct24 = 'Oct 24';
+  static const String dec24 = 'Dec 24';
+  static const String jan25 = 'Jan 25';
+  static const String feb25 = 'Feb 25';
+  static const String mar25 = 'Mar 25';
+  static const String apr25 = 'Apr 25';
+  static const String rooms = 'Rooms';
+  static const String five = '5';
+  static const String twoOfThemRequiresAction = '2 of them requires action';
+  static const String persons = 'Persons';
+  static const String plusTwo = '+2';
+  static const String plants = 'Plants';
+  static const String fortyThree = '43';
+
+
+  /// add screen
+  static const String totalNumberOfPeople = 'Total number of people';
+  static const String totalNumberOfRooms = 'Total number of rooms';
+  static const String totalArea = 'Total area (sqft.)';
+  static const String totalAC = 'Total AC';
+  static const String totalRefrigerator = 'Total Refrigerator';
+  static const String totalComputers = 'Total Computers';
+  static const String totalIndoorPlants = 'Total Indoor Plants';
+  static const String totalKitchenBurner = 'Total Kitchen Burner';
+  static const String calculateCarbonEmission = 'Calculate Carbon Emission';
+
+  ///result screen
+  static const String oneThousandThreeHundred = '1300';
+  static const String veryUnhealthy = 'Very Unhealthy: Your office is emitting a lot of Carbon Dioxide';
+  static const String plant = 'Plant';
+  static const String indoorPlants = '189 indoor plants';
+  static const String officeEmitsCO2 = 'Your office emits 2549ppm Carbon dioxide everyday';
+  static const String planted = 'Planted';
+  static const String plantedRatio = '23/189';
+  static const String done = 'Done';
 }
