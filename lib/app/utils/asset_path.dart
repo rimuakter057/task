f4c62671b7c3566aca7e81c8a0b460893e0e4bc1
rimuakter057 <rimu.appdeveloper@gmail.com>
2025-04-28 10:46:44 +0600

    add asset path

--- lib/app/utils/asset_path.dart
+++ lib/app/utils/asset_path.dart
@@ -1,15 +1,23 @@
 class AssetImages{
-  static const String basePath = 'assets/images/';
+  static const String basePath = 'assets/';
 
-  static const String infoBackImage = '${basePath}info_back_container.jpg';
- static const String appbarIcon = '${basePath}app_bar_icon.png';
-  static const String alarm = '${basePath}alarm.png';
-  static const String tasbihCount = '${basePath}tasbih_count.png';
-  static const String profileImage = '${basePath}profile_image.jpg';
-  static const String dua = '${basePath}dua.png';
-  static const String name = '${basePath}name.png';
-  static const String tasbih = '${basePath}tasbih.png';
-  static const String jakat = '${basePath}jakat.png';
-  static const String duaPlay = '${basePath}dua_play.png';
+  //icon
+  static const String logoIcon = '${basePath}icons/logo.png';
+  static const String facebookIcon = '${basePath}icons/facebook.png';
+  static const String googleIcon = '${basePath}icons/google.png';
+  static const String appleIcon = '${basePath}icons/apple.png';
+  static const String microsoftIcon = '${basePath}icons/microsoft.png';
+
+  //image
+  static const String flagImage = '${basePath}images/flag.png';
+  static const String homeImage = '${basePath}images/home.png';
+  static const String leafImage = '${basePath}images/leaf.png';
+  static const String officeImage = '${basePath}images/office.png';
+
+  //profile
+  static const String profile = '${basePath}profiles/profile.png';
+  static const String profile1 = '${basePath}profiles/profile1.png';
+  static const String profile2 = '${basePath}profiles/profile2.png';
+  static const String profile3 = '${basePath}profiles/profile3.png';
 }
 
